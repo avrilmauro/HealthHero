f54ff448e37d9fa62c087f28fe14b36daff84060
evankeister <90649291+evankeister@users.noreply.github.com>
2023-04-16 19:04:59 -0400

    EK - 7:04 PM - Sunday - 4/16

--- flask-app/src/patients/patients.py
+++ flask-app/src/patients/patients.py
@@ -8,7 +8,7 @@ patients = Blueprint('patients', __name__)
 
 # Get all of the doctor names in the database, and if they are MD or DO
 @patients.route('/doctornames', methods=['GET'])
-def get_products():
+def get_doctor_names():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
@@ -101,14 +101,13 @@ def make_an_appoitment():
     
     req_data = request.get_json()
     
-    appointmentDate = req_data['DatePicker1']
-    date_obj = datetime.datetime.strptime(appointmentDate, '%Y-%m-%dT%H:%M:%S.%fZ')
+    appointmentDate = req_data['AppointmentDate']
     doctor_id = req_data['DoctorID']
     SSN = req_data['SSN_2']
     Cost = round(random.uniform(1000.00, 10000.00), 2)
     
     insert_stmt = 'INSERT INTO Doctor_treats_Patient (DoctorID,SSN,AppointmentDate,Cost) '
-    insert_stmt += 'VALUES (' + str(doctor_id) + ", " + str(SSN) + ', "' + str(date_obj) + '", ' + str(Cost) + ")"
+    insert_stmt += 'VALUES (' + str(doctor_id) + ", " + str(SSN) + ', "' + appointmentDate + '", ' + str(Cost) + ")"
     
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
@@ -149,4 +148,51 @@ def get_prescription_list():
        json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+# Get more information about a specific patient
+@patients.route('/delete_appointment', methods=['DELETE'])
+def delete_appoitment():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN_1 = req_data['SSN_3']
+    doctor_ID = req_data['DoctorID_3']
+    appointment_data = req_data['AppointmentDate']
+
+    query = 'Delete From Doctor_treats_Patient '
+    query += 'WHERE SSN = ' + str(SSN_1) + ' AND DoctorID = ' + str(doctor_ID) + " AND AppointmentDate = '" + appointment_data + "'"
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully deleted the appoitment'
+
+
+# Get more information about a specific patient
+@patients.route('/update_email', methods=['PUT'])
+def update_email():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN = req_data['SSN_email']
+    new_email = req_data['new_email']
+    old_emal = req_data['old_email']
+
+    query = 'UPDATE Emails '
+    query += "SET Email = '" + new_email + "' "
+    query += 'WHERE SSN = ' + str(SSN) + " AND Email = '" + old_emal + "'"
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully updated email'
+
 
