7492c5bcc1e5b8c5f2a3f9aacebe1613179d8a5a
avrilmauro <avfielle@gmail.com>
2023-04-17 16:24:22 -0400

    4:24pm

--- flask-app/src/doctors/doctors.py
+++ flask-app/src/doctors/doctors.py
@@ -11,18 +11,17 @@ def get_patient_name_list():
     
     req_data = request.get_json()
     
-    DoctorID = req_data['DoctorID']
+    DoctorID = req_data['DoctorID_patient_names']
     
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
     # use cursor to query the database for a list of products
-
-    query = 'SELECT Patient.fName as FirstName, Patient.lName as LastName, AppointmentDate, DOB '
+    
+    query = 'Select Patient.fName as PatientFirstName, Patient.lName as PatientLirstName, SSN, DOB '
     query += 'FROM Doctor join Doctor_treats_Patient using(DoctorID) '
     query += 'join Patient using(SSN) '
-    query += 'Where DoctorID = ' + str(DoctorID) + ' '
-    query += 'ORDER BY AppointmentDate'
+    query += 'Where Doctor.DoctorID = ' + str(DoctorID)
     
     cursor.execute(query)
 
@@ -53,6 +52,7 @@ def get_patient_medical_condtions():
     DoctorID = req_data['DoctorID_Med_Hist']
     PatientFirstName = req_data['Patient_FirstName']
     PatientLastName = req_data['Patient_LastName']
+    PatientSSN = req_data['Patient_SSN']
     
     # get a cursor object from the database
     cursor = db.get_db().cursor()
@@ -65,7 +65,7 @@ def get_patient_medical_condtions():
     query += 'join Patient_diagnosed_MedicalCondition PdMC on Patient.SSN = PdMC.SSN '
     query += 'join MedicalCondition using(ConditionID) '
     query += 'Where Patient.SSN = (SELECT SSN FROM Patient '
-    query += 'Where (Patient.fName = "' + PatientFirstName + '" AND Patient.lName = "' + PatientLastName + '") AND (Doctor.DoctorID = ' + str(DoctorID) + '))'
+    query += 'Where (Patient.SSN = ' + str(PatientSSN) + ') AND (Doctor.DoctorID = ' + str(DoctorID) + '))'
     
     cursor.execute(query)
 
@@ -85,3 +85,178 @@ def get_patient_medical_condtions():
        json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+ 
+ # erfef
+# Daignose a patient with a new medical condition
+@doctors.route('/view_medical_conditions_in_database', methods=['GET'])
+def view_medical_conditions_in_database():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    query = 'Select * From MedicalCondition'
+    
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+ 
+ # Daignose a patient with a new medical condition
+@doctors.route('/diagnose', methods=['POST'])
+def diagnose():
+    req_data = request.get_json()
+    
+    ConditionID = req_data['ConditionID']
+    SSN = req_data['SSN_diagnose']
+    
+    insert_stmt = 'INSERT INTO Patient_diagnosed_MedicalCondition (ConditionID, SSN) '
+    insert_stmt += 'VALUES (' + str(ConditionID) + ', ' + str(SSN) + ')'
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_stmt)
+    db.get_db().commit()
+    
+    output = 'Successfully diagnose patinet: ' + str(SSN) + ', with condition: ' + str(ConditionID)
+    return output
+ 
+ 
+ # Cancel an appoitment with a Patient
+@doctors.route('/cancel_appointment', methods=['DELETE'])
+def cancel_appointment():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN_1 = req_data['SSN_delete']
+    doctor_ID = req_data['DoctorID_delete']
+    appointment_data = req_data['AppointmentDate_delete']
+
+    query = 'Delete From Doctor_treats_Patient '
+    query += 'WHERE SSN = ' + str(SSN_1) + ' AND DoctorID = ' + str(doctor_ID) + " AND AppointmentDate = '" + appointment_data + "'"
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully canceled the appointment'
+ 
+ 
+ # Get more information about a specific patient
+@doctors.route('/update_specialty', methods=['PUT'])
+def update_specialty():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    DoctorID = req_data['DoctorID_update_specialty']
+    Specialty = req_data['Specialty_update']
+
+    query = 'UPDATE Doctor '
+    query += "SET Specialty = '" + Specialty + "' "
+    query += 'WHERE DoctorID = ' + str(DoctorID)
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully changed specialty to: ' + str(Specialty) + ', Doctor: ' + str(DoctorID)
+ 
+ 
+ 
+ # View the medications that are being prescribed to one of your patients
+@doctors.route('/view_medication_prescribed_to_patient', methods=['GET'])
+def view_medication_prescribed_to_patient():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+    
+    req_data = request.get_json()
+    DoctorID = req_data['DoctorID_medications']
+    SSN = req_data['SSN_medications']
+
+    # use cursor to query the database for a list of products
+    query = 'Select MedicationCommonName, BrandName, Dosage, '
+    query += 'GenericName, Med_Description, DoctorID, Patient.SSN '
+    query += 'FROM Doctor join Doctor_treats_Patient using(DoctorID) '
+    query += 'join Patient using(SSN) '
+    query += 'join Prescription using(DoctorID) '
+    query += 'join Medication using(MedID) '
+    query += 'WHERE Patient.SSN = ' + str(SSN) + ' AND Doctor.DoctorID = ' + str(DoctorID)
+    
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+ 
+ 
+# View the medications that are being prescribed to one of your patients
+@doctors.route('/see_what_pharmacies_have_medications_for_patient', methods=['GET'])
+def see_what_pharmacies_have_medications_for_patient():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+    
+    req_data = request.get_json()
+    DoctorID = req_data['DoctorID_meds_pharms']
+    SSN = req_data['SSN_meds_pharms']
+
+    # use cursor to query the database for a list of products
+    query = 'Select CompanyName, City, State, Zip, P.Phone '
+    query += 'FROM Doctor join Doctor_treats_Patient using(DoctorID) '
+    query += 'join Patient using(SSN) '
+    query += 'join Prescription using(DoctorID) '
+    query += 'join Medication using(MedID) '
+    query += 'join Pharmacy_contains_Medication using(MedID) '
+    query += 'join Pharmacy P on Pharmacy_contains_Medication.PharmID = P.PharmID '
+    query += 'WHERE Patient.SSN = ' + str(SSN) + ' AND Doctor.DoctorID = ' + str(DoctorID)
+
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+ 
+ 
--- flask-app/src/patients/patients.py
+++ flask-app/src/patients/patients.py
@@ -8,7 +8,7 @@ patients = Blueprint('patients', __name__)
 
 # Get all of the doctor names in the database, and if they are MD or DO
 @patients.route('/doctornames', methods=['GET'])
-def get_products():
+def get_doctor_names():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
 
@@ -37,11 +37,15 @@ def get_products():
 def view_medical_conditions():
     # get a cursor object from the database
     cursor = db.get_db().cursor()
+    
+    req_data = request.get_json()
+    SSN = req_data['SSN_view_medical_conditinos']
 
     # use cursor to query the database for a list of products
     query =  'Select ConditionName, DateDiscovered, TreatmentOptions, Transmissible, Causes, Severity '
     query += 'FROM Patient join Patient_diagnosed_MedicalCondition PdMC using(SSN) '
-    query += 'join MedicalCondition using(ConditionID)'
+    query += 'join MedicalCondition using(ConditionID) '
+    query += 'WHERE Patient.SSN = ' + str(SSN)
     cursor.execute(query)
 
     # grab the column headers from the returned data
@@ -62,13 +66,13 @@ def view_medical_conditions():
     return jsonify(json_data)
 
 # Get all of the doctor names in the database, and if they are MD or DO
-@patients.route('/view_your_appoitments', methods=['GET'])
-def view_your_appoitments():
+@patients.route('/view_your_appointments', methods=['GET'])
+def view_your_appointments():
      # get a cursor object from the database
     cursor = db.get_db().cursor()
     
     req_data = request.get_json()
-    patient_SSN = req_data['SSN']
+    patient_SSN = req_data['SSN_appointment_view']
 
     # use cursor to query the database for a list of products
     query = 'Select AppointmentDate, Doctor.fName as DoctorFirstName, Doctor.lName as DoctorLastName, Cost '
@@ -95,26 +99,25 @@ def view_your_appoitments():
 
     return jsonify(json_data)
 
-# Get all of the doctor names in the database, and if they are MD or DO
-@patients.route('/make_an_appoitment', methods=['POST'])
-def make_an_appoitment():
-    
+# Schedule an appointment for a Patient
+@patients.route('/make_an_appointment', methods=['POST'])
+def make_an_appointment():
     req_data = request.get_json()
     
-    appointmentDate = req_data['DatePicker1']
-    date_obj = datetime.datetime.strptime(appointmentDate, '%Y-%m-%dT%H:%M:%S.%fZ')
-    doctor_id = req_data['DoctorID']
-    SSN = req_data['SSN_2']
+    appointmentDate = req_data['AppointmentDate']
+    doctor_id = req_data['DoctorID_make_appointment']
+    SSN = req_data['SSN_make_appointment']
     Cost = round(random.uniform(1000.00, 10000.00), 2)
     
     insert_stmt = 'INSERT INTO Doctor_treats_Patient (DoctorID,SSN,AppointmentDate,Cost) '
-    insert_stmt += 'VALUES (' + str(doctor_id) + ", " + str(SSN) + ', "' + str(date_obj) + '", ' + str(Cost) + ")"
+    insert_stmt += 'VALUES (' + str(doctor_id) + ", " + str(SSN) + ', "' + appointmentDate + '", ' + str(Cost) + ")"
     
     cursor = db.get_db().cursor()
     cursor.execute(insert_stmt)
     db.get_db().commit()
-
-    return 'Success'
+    
+    output = 'Successfully scheduled an appoitment with ' + str(doctor_id) + ' on + ' + appointmentDate + "."
+    return output
 
 # Get more information about a specific patient
 @patients.route('/get_prescription_list', methods=['GET'])
@@ -125,7 +128,7 @@ def get_prescription_list():
     # use cursor to query the database for a list of products
     
     req_data = request.get_json()
-    SSN_1 = req_data['SSN_3']
+    SSN_1 = req_data['SSN_get_prescription']
 
     query = 'Select MedicationCommonName, Dosage '
     query += 'FROM Patient join Prescription using (SSN) '
@@ -149,4 +152,87 @@ def get_prescription_list():
        json_data.append(dict(zip(column_headers, row)))
 
     return jsonify(json_data)
+
+# Get more information about a specific patient
+@patients.route('/delete_appointment', methods=['DELETE'])
+def delete_appoitment():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN_1 = req_data['SSN_delete']
+    doctor_ID = req_data['DoctorID_delete']
+    appointment_data = req_data['AppointmentDate_delete']
+
+    query = 'Delete From Doctor_treats_Patient '
+    query += 'WHERE SSN = ' + str(SSN_1) + ' AND DoctorID = ' + str(doctor_ID) + " AND AppointmentDate = '" + appointment_data + "'"
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully deleted the appoitment'
+
+
+# Get more information about a specific patient
+@patients.route('/update_email', methods=['PUT'])
+def update_email():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN = req_data['SSN_email']
+    new_email = req_data['new_email']
+    old_emal = req_data['old_email']
+
+    query = 'UPDATE Emails '
+    query += "SET Email = '" + new_email + "' "
+    query += 'WHERE SSN = ' + str(SSN) + " AND Email = '" + old_emal + "'"
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Successfully updated email'
+
+# Get more information about a specific patient
+@patients.route('/get_insurance_info', methods=['GET'])
+def get_insurance_info():
+   # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    
+    req_data = request.get_json()
+    SSN = req_data['SSN_insurance']
+
+    query = 'Select CompanyName, Coverage, Expiration, PlanType '
+    query += 'FROM Patient join Insurance using (CompanyID) '
+    query += 'WHERE Patient.SSN = ' + str(SSN)
+    
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+       json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+
 
